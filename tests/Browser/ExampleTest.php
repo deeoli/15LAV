075e3e6ed7d8de6c2a1958c6c8350a038be8e620
A O <you@example.com>
2022-01-31 03:10:02 +0000

    added

---
+++ tests/Browser/ExampleTest.php
@@ -0,0 +1,26 @@
+<?php
+//
+//namespace Tests\Browser;
+//
+//use Illuminate\Foundation\Testing\DatabaseMigrations;
+//use Laravel\Dusk\Browser;
+//use Tests\DuskTestCase;
+//
+//class ExampleTest extends DuskTestCase
+//{
+//    /**
+//     * A basic browser test example.
+//     *
+//     * @return void
+//     */
+//    public function testBasicExample()
+//    {
+//        echo "pre tesst";
+//        $this->browse(function (Browser $browser) {
+//            $browser->visit('/')
+//                    ->assertSee('Laravel');
+//        });
+//
+//        echo "post tesst";
+//    }
+//}
